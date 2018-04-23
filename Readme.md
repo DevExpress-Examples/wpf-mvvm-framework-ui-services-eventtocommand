aba8fcd38364e7cd3464fdbc0d0ac23272de5591
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Use EventToCommand
+
+
+This example demonstrates how to use the EventToCommand.<br>Review <a href="https://documentation.devexpress.com/WPF/17369/Common-Concepts/MVVM-Framework/Behaviors/Predefined-Set/EventToCommand">this documentation topic</a> to learn more.
+
+<br/>
+
+
---
+++ VB/Common/MouseEventArgsConverter.vb
@@ -0,0 +1,16 @@
+﻿Imports DevExpress.Mvvm.UI
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Input
+Imports System.Linq
+
+Namespace Example.Common
+    Public Class ListBoxEventArgsConverter
+        Inherits EventArgsConverterBase(Of MouseEventArgs)
+
+        Protected Overrides Function Convert(ByVal sender As Object, ByVal args As MouseEventArgs) As Object
+            Dim element = LayoutTreeHelper.GetVisualParents(DirectCast(args.OriginalSource, DependencyObject), DirectCast(sender, DependencyObject)).OfType(Of ListBoxItem).FirstOrDefault()
+            Return If(element IsNot Nothing, element.DataContext, Nothing)
+        End Function
+    End Class
+End Namespace
